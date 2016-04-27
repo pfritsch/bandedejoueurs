99c6f57283cf9e93b32db1300bfa5a7d19b307dc
Pierre <fritschpierre@gmail.com>
2016-04-27 18:01:04 +0200

    Fast Render

--- init.js
+++ init.js
@@ -31,12 +31,6 @@ if (Meteor.isClient) {
 }
 if (Meteor.isServer) {
 
-  // Listen to incoming HTTP requests, can only be used on the server
-  WebApp.rawConnectHandlers.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    return next();
-  });
-
   Meteor.startup(function () {
 
     // Add Facebook configuration entry
