e9617510096adb0ea5146928c052e4b3c9d8e55c
Pierre Fritsch <fritsch@puzzle.ch>
2016-06-01 16:29:26 +0200

    Change daily news time

--- server/newsletter.js
+++ server/newsletter.js
@@ -2,8 +2,7 @@ SyncedCron.add({
   name: 'Send news',
   schedule: function(parser) {
     // return parser.text('every 30 secs');
-    // return parser.text('on Monday at 10:00 am every week');
-    return parser.text('at 8:00 am');
+    return parser.text('at 18:00'); // Every day at 18:00
   },
   job: function() {
     Meteor.call('sendNews');
